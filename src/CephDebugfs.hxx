0d598902e8fd99fbddf3ad97781583ccfe241cc2
Max Kellermann <mk@cm4all.com>
2026-08-17 22:42:06 +0200

    KernelExporter: move code to CephDebugfs.cxx

---
+++ src/CephDebugfs.hxx
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <max.kellermann@ionos.com>
+
+#pragma once
+
+class BufferedOutputStream;
+
+void
+ExportCeph(BufferedOutputStream &os);
